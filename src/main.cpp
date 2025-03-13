ad334e703487704cbd255a349e2fc7c0d759be4e
Connor Garey <connorgarey1999@gmail.com>
2025-03-13 12:16:47 +0000

    changed inf to targetFile for clarity.

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,20 @@
 #include <string>
 
 void getFile () {
-  std::ifstream inf{"/etc/hostname"};
+  std::ifstream targetFile{"/etc/hostname"};
   // Error handling for not being able to open.
-  if (!inf) {
+  if (!targetFile) {
     std::cerr << "Failed to open file!";
     std::exit(1);
   }
   std::string strInput{};
-  while (inf >> strInput) {
+  while (targetFile >> strInput) {
     std::cout << strInput << "\n";
   }
+}
+
+void printToScreen() {
+
 }
 
 int main (int argc, char *argv[]) {
