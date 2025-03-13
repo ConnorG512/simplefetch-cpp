1075626112edfa1ddcbcb4c3fe475a911a280cb6
Connor Garey <connorgarey1999@gmail.com>
2025-03-13 21:56:52 +0000

    Added USER, SOFTWARE and HARDWARE tags to main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 #include "file.cpp"
 
 int main (int argc, char *argv[]) {
-
+  std::cout << "---USER:\n";
   File hostname;
   hostname.init("Hostname:", "/etc/hostname");
   hostname.printTitleToScreen();
   hostname.printFileToScreen();
-  
+  std::cout << "---SOFTWARE:\n";
   FileByWord kernel;
   kernel.init("Kernel:","/proc/version", 2);
   kernel.printTitleToScreen();
   kernel.printFileToScreen();
-
+  std::cout << "---HARDWARE:\n";
   FileByWord memTotal;
   memTotal.init("Memory Total (kB):","/proc/meminfo",1);
   memTotal.printTitleToScreen();
