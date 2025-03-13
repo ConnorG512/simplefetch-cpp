559ac53ac568deba0ecbf2bbc2cdea08e8ab13ec
Connor Garey <connorgarey1999@gmail.com>
2025-03-13 12:47:51 +0000

    Created class and added as a member function.

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,25 @@
 #include <iostream>
 #include <string>
 
-void printFileToScreen () {
-  std::ifstream targetFile{"/etc/hostname"};
-  // Error handling for not being able to open.
-  if (!targetFile) {
-    std::cerr << "Failed to open file!";
-    std::exit(1);
+class File {
+  std::string filePath;
+  
+  void printFileToScreen () {
+    std::ifstream targetFile{filePath};
+    // Error handling for not being able to open.
+    if (!targetFile) {
+      std::cerr << "Failed to open file!";
+      std::exit(1);
+    }
+    std::string strInput{};
+    while (std::getline(targetFile, strInput)) {
+      std::cout << strInput << "\n";
+    }
   }
-  std::string strInput{};
-  while (std::getline(targetFile, strInput)) {
-    std::cout << strInput << "\n";
-  }
-}
+};
+
 
 int main (int argc, char *argv[]) {
-  printFileToScreen();
   return 0;
 }
 
