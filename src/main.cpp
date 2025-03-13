f30f2bb3ddaa5e62f5dab87fb18b1ea27efb729b
Connor Garey <connorgarey1999@gmail.com>
2025-03-13 14:55:12 +0000

    main.cpp cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 #include "file.cpp"
 
 int main (int argc, char *argv[]) {
-  // Hostname object build from File class
   File hostname {"Hostname:","/etc/hostname"};
   hostname.printTitleToScreen(); 
   hostname.printFileToScreen();
+
   File version {"Version:","/proc/version"};
   version.printTitleToScreen();
   version.printFileToScreen();
