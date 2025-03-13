bbbeacb1731c14bc6fbee9ab9241229762fe1d6f
Connor Garey <connorgarey1999@gmail.com>
2025-03-13 14:52:28 +0000

    Seperate file for File struct.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,6 @@
 #include <cstdlib>
-#include <fstream>
-#include <iostream>
-#include <string>
 
-struct File {
-  std::string title{};
-  std::string filePath{};
-  
-  void printTitleToScreen() {
-    std::cout << title << " ";
-  }
-  void printFileToScreen () {
-    std::ifstream targetFile{filePath};
-    // Error handling for not being able to open.
-    if (!targetFile) {
-      std::cerr << "Failed to open file!";
-      std::exit(1);
-    }
-    std::string strInput{};
-    // Print file contents
-    while (std::getline(targetFile, strInput)) {
-      std::cout << strInput << "\n";
-    }
-  }
-};
+#include "file.cpp"
 
 int main (int argc, char *argv[]) {
   // Hostname object build from File class
