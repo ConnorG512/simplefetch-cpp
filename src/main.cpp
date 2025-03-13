9a4c85b4d289acf3978596b5ceb653930c5aca79
Connor Garey <connorgarey1999@gmail.com>
2025-03-13 16:20:37 +0000

    Added memTotal and memFree.

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,17 @@ int main (int argc, char *argv[]) {
   hostname.printTitleToScreen(); 
   hostname.printFileToScreen();
 
-  File version {"Version:","/proc/version",2};
+  File version {"Kernel:","/proc/version",2};
   version.printTitleToScreen();
   version.printFileToScreen();
+
+  File memTotal {"Memory Total (kB):", "/proc/meminfo",1};
+  memTotal.printTitleToScreen();
+  memTotal.printFileToScreen();
+  
+  File memFree {"Memory Free (kB):", "/proc/meminfo",4};
+  memFree.printTitleToScreen();
+  memFree.printFileToScreen();
   return 0;
 }
 
