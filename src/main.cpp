afdab3c6183dc96d6371ba6ae38e1dbd26d68554
Connor Garey <connorgarey1999@gmail.com>
2025-03-14 13:01:16 +0000

    Added cyan ansii escape code for title text

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,29 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <ostream>
 
 #include "file.cpp"
 
 int main (int argc, char *argv[]) {
-  std::cout << "---USER:\n";
+  std::cout << "\033[96m---USER:\033[0m" << std::endl;
   File hostname {"Hostname:", "/etc/hostname"};
   hostname.printTitleToScreen();
   hostname.printFileToScreen();
-  std::cout << "---SOFTWARE:\n";
+
+  std::cout << "\033[96m---SOFTWARE:\033[0m" << std::endl;
   FileByWord kernel {"Kernel:", "/proc/version", 2};
   kernel.printTitleToScreen();
   kernel.printFileToScreen();
-  std::cout << "---HARDWARE:\n";
+
+  std::cout << "\033[96m---HARDWARE:\033[0m" << std::endl;
   FileByWord memTotal {"Memory Total (kB):", "/proc/meminfo", 1};
   memTotal.printTitleToScreen();
   memTotal.printFileToScreen();
+
   FileByWord memFree {"Memory Free (kB):", "/proc/meminfo", 4};
   memFree.printTitleToScreen();
   memFree.printFileToScreen();
-
   return 0;
 }
 
