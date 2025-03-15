4de6aa6a7b80e0c0bbbc4722c33a2ed3472253cd
Connor Garey <connorgarey1999@gmail.com>
2025-03-15 14:19:43 +0000

    Added bright colour escapes to enum.

--- src/title.h
+++ src/title.h
@@ -10,14 +10,14 @@ class Title {
   private:
     std::string m_printed_text{};
     enum m_TextColor {
-      black,
-      red,
-      green,
-      yellow,
-      blue,
-      magenta,
-      cyan,
-      white,
+      bright_black = 90,
+      bright_red = 91,
+      bright_green = 92,
+      bright_yellow = 93,
+      bright_blue = 94,
+      bright_magenta = 95,
+      bright_cyan = 96,
+      bright_white = 97,
     };
   
   public:
