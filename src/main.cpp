bfbfe422511f7d88532c683a70580f2fb39cad25
Connor Garey <connorgarey1999@gmail.com>
2025-03-15 19:27:49 +0000

    Added colour change function to all titles.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <cstdio>
 #include <cstdlib>
-#include <iostream>
-#include <ostream>
 
 #include "file.h"
 #include "title.h"
 
 int main (int argc, char *argv[]) {
-  std::cout << "\033[96m---USER:\033[0m" << std::endl;
+  Title title_hostname {"USER:", Title::bright_green};
+  title_hostname.printLineToScreen();
   File hostname {"Hostname:", "/etc/hostname"};
   hostname.printTitleToScreen();
   hostname.printFileToScreen();
 
-  Title software {"--SOFTWARE", Title::bright_black};
-  software.printLineToScreen();
+  Title title_software {"SOFTWARE:", Title::bright_black};
+  title_software.printLineToScreen();
   FileByWord kernel {"Kernel:", "/proc/version", 2};
   kernel.printTitleToScreen();
   kernel.printFileToScreen();
 
-  std::cout << "\033[96m---HARDWARE:\033[0m" << std::endl;
+  Title title_hardware {"HARDWARE:", Title::bright_magenta};
+  title_hardware.printLineToScreen();
   FileByWord memTotal {"Memory Total (kB):", "/proc/meminfo", 1};
   memTotal.printTitleToScreen();
   memTotal.printFileToScreen();
@@ -26,6 +26,8 @@ int main (int argc, char *argv[]) {
   FileByWord memFree {"Memory Free (kB):", "/proc/meminfo", 4};
   memFree.printTitleToScreen();
   memFree.printFileToScreen();
+
+  // Exit 
   return 0;
 }
 
--- src/title.cpp
+++ src/title.cpp
@@ -1,4 +1,5 @@
 #include "title.h"
+#include <cstdio>
 #include <iostream>
 #include <string>
 
@@ -6,5 +7,5 @@ Title::Title(std::string printed_text, TextColor text_color )
   : m_printed_text { printed_text }, m_text_color { text_color } {}
 
 void Title::printLineToScreen() {
-  std::cout << "\033[" << m_text_color << "m---SOFTWARE:\033[0m" << std::endl;
+  std::cout << "\033[" << m_text_color << "m" << m_printed_text << "\033[0m" << "\n";
 }
