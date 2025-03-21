f60909a74b5a62858a95da2d106118610fc0646a
Connor Garey <connorgarey1999@gmail.com>
2025-03-21 20:48:38 +0000

    pinting all parts of uname struct.

--- src/uname.cpp
+++ src/uname.cpp
@@ -5,9 +5,12 @@
 
 void Uname::getUnameInformation() {
   uname(&m_uname_buffer);  
-  std::cout << m_uname_buffer.release << std::endl;
-  std::cout << m_uname_buffer.machine << std::endl;
   std::cout << m_uname_buffer.version << std::endl;
+  std::cout << m_uname_buffer.machine << std::endl;
+  std::cout << m_uname_buffer.release << std::endl;
+  std::cout << m_uname_buffer.domainname << std::endl;
+  std::cout << m_uname_buffer.nodename << std::endl;
+  std::cout << m_uname_buffer.sysname << std::endl;
 }
 Uname::Uname() {
   getUnameInformation();
