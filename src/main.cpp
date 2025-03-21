c9e59477652583e1b4db089d884d277db4891b34
Connor Garey <connorgarey1999@gmail.com>
2025-03-21 15:31:29 +0000

    Additions to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,34 @@
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <ostream>
 
 #include "filesystem.h"
 #include "printer.h"
 
 int main (int argc, char *argv[]) {
-  Printer user {"USER: ", Printer::bright_cyan};
-  Filesystem user_fs {"/etc/hostname", 128, 0};
-  Filesystem uptime_fs {"/proc/uptime", 30, 0};
-  Printer hardware_printer {"HARDWARE: ", Printer::bright_magenta};
-  Filesystem meminfo_fs {"/proc/meminfo", 8, 16};
+  // USER
+  {
+    Printer user_printer {"USER: ", Printer::bright_cyan};
+    std::cout << "Hostname: ";
+    Filesystem user_fs {"/etc/hostname", 128, 0};
+    std::cout << "Uptime: ";
+    Filesystem uptime_fs {"/proc/uptime", 30, 0};
+  }
+  // HARDWARE
+  {
+    Printer hardware_printer {"HARDWARE: ", Printer::bright_red};
+    std::cout << "Version: ";
+    Filesystem kernelinfo_fs {"/proc/version", 14, 0xE};
+    std::cout << std::endl;
+    std::cout << "Total memory: ";
+    Filesystem meminfo_fs {"/proc/meminfo", 8, 16};
+    std::cout << std::endl;
+  }
+  // NETWORK
+  {
+    Printer network_printer {"NETWORK: ", Printer::bright_magenta};
+  }
   // Exit 
   return 0;
 }
